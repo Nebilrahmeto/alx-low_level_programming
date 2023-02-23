734f1c574f6435242cf18537a71e020a1ce0b1ff
Nebilrahmeto <rahmetonebil@gmail.com>
2023-02-23 12:04:39 -0800

    number

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,11 +1,15 @@
 #include "main.h"
+
 /**
- *print_numbers - function that print all numbers from
- *Return: returns nothing
+ * print_numbers - function that print all numbers from
+ *
+ * Return: returns nothing
 */
+
 void print_numbers(void)
 {
-	int number = 48;
+	int number;
+
 	for (number = 48; number < 58; number++)
 {
 	_putchar(number);
