67c1ecd9081a4119177fede5e42650c931f0ba9e
Nebilrahmeto <rahmetonebil@gmail.com>
2023-02-23 11:59:35 -0800

    numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,7 +10,7 @@ void print_most_numbers(void)
 	int num = 48;
 	for (num = 48; num < 58; num++)
 	{
-		if (num != 50 && num != 52)
+		if ((num != 50) && (num != 52))
 			_putchar(num);
 	}
 	_putchar(10);
